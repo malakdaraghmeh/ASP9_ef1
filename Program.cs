e8497034e83a850c5f9b5e57d8c7866718adf504
malakdaraghmeh <malakdaraghmeh23@gmail.com>
2024-08-08 17:34:22 +0300

    EF1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace task_ef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task_ef.model;
+
+namespace task_ef.context
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;Database=Asp-Ef;Trusted_connection=True;TrustServerCertificate=True");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Department>Departments { get; set; }
+    }
+}
